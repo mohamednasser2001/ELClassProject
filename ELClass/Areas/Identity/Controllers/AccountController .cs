413112636be21b790cea4c5eb142cfe104262350
mohamednasser2001 <mmm.zzz155mody@gmail.com>
2025-12-31 17:00:21 +0200

    CreateIdentity

---
+++ ELClass/Areas/Identity/Controllers/AccountController .cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ELClass.Areas.Identity.Controllers
+{
+    [Area("Identity")]
+    public class AccountController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
