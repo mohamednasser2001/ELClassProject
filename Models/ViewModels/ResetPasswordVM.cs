6de7e1707f5ed0f92dcccc0a804b3cdf5924ea3a
mohamednasser2001 <mmm.zzz155mody@gmail.com>
2026-01-04 18:11:45 +0200

    AddForgetPasswordAndResendEmailAndConfirmEmail

---
+++ Models/ViewModels/ResetPasswordVM.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Models.ViewModels
+{
+    public class ResetPasswordVM
+    {
+        [Required]
+        public string UserId { get; set; }
+
+        [Required]
+        public string Token { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "Password must be at least 6 characters")]
+        public string NewPassword { get; set; }
+
+        [Required(ErrorMessage = "Confirm Password is required")]
+        [DataType(DataType.Password)]
+        [Compare("NewPassword", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
