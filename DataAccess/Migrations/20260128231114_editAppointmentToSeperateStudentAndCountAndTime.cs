ab8372b53f635906cdb1e1d92cd8e65179f99b07
Ahmed Elbelkasy <Ahmed Elbelkasy@AHMEDELBELKASY>
2026-01-29 02:42:15 +0200

    edit the relation between appointment and student and collect all student in the same appointment and added the attend date

---
+++ DataAccess/Migrations/20260128231114_editAppointmentToSeperateStudentAndCountAndTime.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class editAppointmentToSeperateStudentAndCountAndTime : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExpiryDate",
+                table: "Appointments");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "StudentExpiryDate",
+                table: "StudentAppointments",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StudentExpiryDate",
+                table: "StudentAppointments");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ExpiryDate",
+                table: "Appointments",
+                type: "datetime2",
+                nullable: true);
+        }
+    }
+}
