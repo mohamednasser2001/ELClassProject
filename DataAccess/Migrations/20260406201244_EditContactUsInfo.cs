3e5300dde61e4155eb47d6973b92fdc7151365b3
Ahmed Elbelkasy <Ahmed Elbelkasy@AHMEDELBELKASY>
2026-04-06 22:26:42 +0200

    edit contact us edit navigation link

---
+++ DataAccess/Migrations/20260406201244_EditContactUsInfo.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class EditContactUsInfo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Message",
+                table: "ContactUs");
+
+            migrationBuilder.RenameColumn(
+                name: "Topic",
+                table: "ContactUs",
+                newName: "Course");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Age",
+                table: "ContactUs",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Age",
+                table: "ContactUs");
+
+            migrationBuilder.RenameColumn(
+                name: "Course",
+                table: "ContactUs",
+                newName: "Topic");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Message",
+                table: "ContactUs",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
--- Models/ContactUs.cs
+++ Models/ContactUs.cs
@@ -10,8 +10,8 @@ namespace Models
         public string Name { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
         public string PhoneNumber { get; set; } = string.Empty;
-        public string Topic { get; set; } = string.Empty;
-        public string Message { get; set; } = string.Empty;
+        public int Age { get; set; }
+        public string Course { get; set; } = string.Empty;
         public bool IsReaded { get; set; } = false;
 
     }
